807c27a1b8af0f1b446791d5251f2a9fbf3b28da
guilherme maywald <guilhermemaywald@gmail.com>
2022-09-28 16:15:31 -0300

    Segunda correção

--- assets/js/dom_5.js
+++ assets/js/dom_5.js
@@ -14,8 +14,8 @@ let receiveEmail = document.getElementById('email-client');
 let receivePhone = document.getElementById('phone-client');
 let messageClient = document.getElementById('message');
 let listClient = document.getElementById('registered');
-let btnPressSave = document.getElementById('btn_save');
-let btnPressConclude = document.getElementById('btn_conclude');
+let btnPressSave = document.getElementById('btn-save');
+let btnPressConclude = document.getElementById('btn-conclude');
 let arrFields = [];
 
 let validateFields = (field) => {
@@ -51,4 +51,20 @@ function receivedFields(){
     console.log(arrFields);
 }
 
+function displayList(element, key) {
+    console.log(key, element.name);
+    console.log(key, element.email);
+    console.log(key, element.telephone);
+}
+
+function completeRegistration() {
+    let registrationCompleted = document.getElementsByClassName('formulary');
+    let listCompleted = document.getElementsByClassName('message-registration');
+
+    registrationCompleted.setAttribute('class', 'inactive');
+    listCompleted.setAttribute('class', 'active');
+    arrFields.forEach(displayList);
+}
+
 btnPressSave.addEventListener("click", receivedFields);
+btnPressConclude.addEventListener("click", completeRegistration);
